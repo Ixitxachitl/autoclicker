dd53f93e12f63035741ce1a5bba4541e4919a12e
nkoeppel <kramerfm@gmail.com>
2016-02-12 16:11:21 -0800

    bug fix

--- src/autoclick.py
+++ src/autoclick.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pyautogui, time, threading
+import pyautogui, time, threading, sys
 from tkinter import Tk, BOTH, RIGHT, LEFT
 from tkinter.ttk import Frame, Button, Style
 import tkinter as tk
@@ -27,9 +27,9 @@ class Example(Frame):
 
         closeButton = Button(self, text="Close", command=self.sysExit)
         closeButton.pack(side=RIGHT, padx=5, pady=5)
-        startButton = Button(self, text="Start", command=self.startClick)
+        startButton = Button(self, text="Start")
         startButton.pack(side=LEFT, padx=5, pady=5)
-        startButton.bind("<Button-1>", self.buttonText)
+        startButton.bind("<Button-1>", self.startClick)
 
     def bindExit(self, e):
         self.sysExit()
@@ -38,23 +38,19 @@ class Example(Frame):
     def sysExit(self):
         global running
         running = 0
-        self.quit()
-        
-    def buttonText(self, event):
-        if event.widget["text"] == "Start":
-            event.widget.config(text="Stop")
-        else:
-            event.widget.config(text="Start")
+        sys.exit()
             
-    def startClick(self):
+    def startClick(self, event):
         global running
         if running == 0:
             running = 1
+            event.widget.config(text="Stop")
             currentMouseX, currentMouseY = pyautogui.position()
             pyautogui.moveTo(currentMouseX+50, currentMouseY+50)
             threading.Thread(target=self.startLoop, args=()).start()
         else:
             running = 0
+            event.widget.config(text="Start")
             time.sleep(0.5)
 
     def startLoop(self):
@@ -63,9 +59,15 @@ class Example(Frame):
            pyautogui.click()
            pyautogui.click(button='middle')
            time.sleep(0.0001)
+        return
 
 def main():
     root = Tk()
+    def on_closing():
+        global running
+        running = 0
+        sys.exit()
+    root.protocol("WM_DELETE_WINDOW",on_closing)    
     root.wm_attributes("-topmost", 1)
     root.resizable(0,0)
     app = Example(root)
